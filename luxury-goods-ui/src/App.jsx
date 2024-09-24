288c36085035cf86983ba38a405df30b1a8d9918
walterthesmart <nwaugowalter@gmail.com>
2024-09-24 08:21:45 +0100

    code for stacks

--- luxury-goods-ui/src/App.jsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <main className="flex h-screen bg-yellow-800">
-      <h1>Luxury Goods</h1>
-      <button>Connect Wallet</button>
-    </main>
-  );
-}

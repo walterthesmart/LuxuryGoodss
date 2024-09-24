cd7f09b8679d3204847892190ae08f76f77e0b54
walterthesmart <nwaugowalter@gmail.com>
2024-09-24 08:55:55 +0100

    Refactor App component to import dependencies and update UI

--- luxury-goods-ui/src/App.jsx
+++ luxury-goods-ui/src/App.jsx
@@ -1,57 +1,54 @@
-async (params) => {
-  import { AppConfig, showConnect, UserSession } from "@stacks/connect";
-  import { useState } from "react";
+import { AppConfig, showConnect, UserSession } from "@stacks/connect";
+import { useState } from "react";
+
+
+
+
+function App() {
+  const appConfig = new AppConfig(["store_write", "publish_data"]);
+  const userSession = new UserSession({ appConfig });
+  const [userAddress, setUserAddress] = useState(function () {
+    return JSON.parse(sessionStorage.getItem("userAddress"));
+  });
+
+
   
-  
-  
-  
-  function App() {
-    const appConfig = new AppConfig(["store_write", "publish_data"]);
-    const userSession = new UserSession({ appConfig });
-    const [userAddress, setUserAddress] = useState(function () {
-      return JSON.parse(sessionStorage.getItem("userAddress"));
+  function handleSubmit() {
+    showConnect({
+      appDetails: {
+        name: "My App",
+        icon: "",
+      },
+      onFinish: function () {
+        let {
+          profile: {
+            stxAddress: { testnet },
+          },
+        } = userSession.loadUserData();
+
+        console.log(userSession.loadUserData());
+
+        setUserAddress(testnet);
+        sessionStorage.clear();
+        sessionStorage.setItem("userAddress", JSON.stringify(testnet));
+      },
+      userSession,
     });
-  
-  
-    
-    function handleSubmit() {
-      showConnect({
-        appDetails: {
-          name: "My App",
-          icon: "",
-        },
-        onFinish: function () {
-          let {
-            profile: {
-              stxAddress: { testnet },
-            },
-          } = userSession.loadUserData();
-  
-          console.log(userSession.loadUserData());
-  
-          setUserAddress(testnet);
-          sessionStorage.clear();
-          sessionStorage.setItem("userAddress", JSON.stringify(testnet));
-        },
-        userSession,
-      });
-    }
-  
-    return (
-      <main>
-        <nav className="bg-yellow-800 py-[2rem] p-[4rem] flex items-center justify-between">
-          <h1 className="text-white text-[2rem]">Luxury Goods UI</h1>
-          <button
-            className="bg-black px-[2rem] py-[.5rem] text-white text-[1.6rem] rounded-[.5rem]"
-            onClick={handleSubmit}
-          >
-            {!userAddress ? "Connect Wallet" : "Wallet Connected"}
-          </button>
-        </nav>
-      </main>
-    );
   }
-  
-  export default App;
-  
+
+  return (
+    <main>
+      <nav className="bg-yellow-800 py-[2rem] p-[4rem] flex items-center justify-between">
+        <h1 className="text-white text-[2rem]">Luxury Goods UI</h1>
+        <button
+          className="bg-black px-[2rem] py-[.5rem] text-white text-[1.6rem] rounded-[.5rem]"
+          onClick={handleSubmit}
+        >
+          {!userAddress ? "Connect Wallet" : "Wallet Connected"}
+        </button>
+      </nav>
+    </main>
+  );
 }
+
+export default App;
